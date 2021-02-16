8c760def7c4feb8cebcc91cfef2ef8fab759b731
Sjaan Hydrick <sjaanahydrick@gmail.com>
2021-02-16 13:32:51 -0800

    Initial ColorPicker copied; Test passing

---
+++ src/components/ColorPicker.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ColorPicker from './ColorPicker';
+
+describe('ColorPicker container', () => {
+  it('changes the color', async() => {
+    render(<ColorPicker />);
+
+    const display = await screen.findByTestId('color');
+
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000'
+    });
+  });
+});
